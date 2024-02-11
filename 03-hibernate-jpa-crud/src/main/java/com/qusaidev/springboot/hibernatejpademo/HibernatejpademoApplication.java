21367cc94e79c898fbf329cc1d0587b4d554d0cd
AlbusTheDev <qusaih42@gmail.com>
2024-02-11 11:30:39 +0300

    Initial commit for part 3

---
+++ 03-hibernate-jpa-crud/src/main/java/com/qusaidev/springboot/hibernatejpademo/HibernatejpademoApplication.java
@@ -0,0 +1,13 @@
+package com.qusaidev.springboot.hibernatejpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatejpademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatejpademoApplication.class, args);
+	}
+
+}
---
+++ 03-hibernate-jpa-crud/src/test/java/com/qusaidev/springboot/hibernatejpademo/HibernatejpademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qusaidev.springboot.hibernatejpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatejpademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
