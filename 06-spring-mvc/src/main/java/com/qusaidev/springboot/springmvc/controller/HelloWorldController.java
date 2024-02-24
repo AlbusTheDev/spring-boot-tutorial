fd14831481f07779f1c33c8b32553143727ddd7e
AlbusTheDev <qusaih42@gmail.com>
2024-02-24 23:54:42 +0300

    using url query parameters in the app

--- 06-spring-mvc/src/main/java/com/qusaidev/springboot/springmvc/controller/HelloWorldController.java
+++ 06-spring-mvc/src/main/java/com/qusaidev/springboot/springmvc/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.qusaidev.springboot.springmvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +16,19 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormTwo")
+    public String shoutName(HttpServletRequest request, Model model) {
+
+        String name = request.getParameter("studentName");
+        name = name.toUpperCase();
+
+        String result = "HELLO " + name;
+
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
