056c9b87dbfcf287c0cb99eaf71c200ad0f510a4
AlbusTheDev <qusaih42@gmail.com>
2024-03-27 12:24:51 +0300

    Added db connection, entities and DAO classes

--- 09-spring-boot-jpa-advanced-mappings/crudemo/src/main/java/com/qusaidev/springboot/crudemo/CrudemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/crudemo/src/main/java/com/qusaidev/springboot/crudemo/CrudemoApplication.java
@@ -1,7 +1,12 @@
 package com.qusaidev.springboot.crudemo;
 
+import com.qusaidev.springboot.crudemo.dao.AppDAO;
+import com.qusaidev.springboot.crudemo.entity.Instructor;
+import com.qusaidev.springboot.crudemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudemoApplication {
@@ -10,4 +15,24 @@ public class CrudemoApplication {
         SpringApplication.run(CrudemoApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+        return runner -> {
+            createInstructor(appDAO);
+        };
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+
+        Instructor instructor =
+                new Instructor("Qusai", "Wael", "qusai@qusaidev.com");
+
+        InstructorDetail instructorDetail =
+                new InstructorDetail("https://youtube.com", "Reading");
+
+        instructor.setInstructorDetail(instructorDetail);
+
+        appDAO.save(instructor);
+        System.out.println("Instructor Saved!");
+    }
 }
---
+++ 09-spring-boot-jpa-advanced-mappings/crudemo/src/main/java/com/qusaidev/springboot/crudemo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package com.qusaidev.springboot.crudemo.dao;
+
+import com.qusaidev.springboot.crudemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor instructor);
+}
